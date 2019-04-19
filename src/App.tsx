521a6184bf677c3dbe458ce70489bfd232896fab
Timefear007 <timefear007@gmail.com>
2019-04-19 15:09:59 +0700

    Add appbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
+import Bar from './components/appbar/Bar';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#CA1C1D"
+    },
+    secondary: {
+      main: "#E5AF70"
+    }
+  }
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      </div>
+      <MuiThemeProvider theme={theme} >
+        <div className="App">
+          <Bar />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
