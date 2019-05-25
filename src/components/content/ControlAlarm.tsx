f7957731ff6440173ae4aaa7ac9fe0ef16b4e41c
Timefear007 <timefear007@gmail.com>
2019-05-25 19:09:23 +0700

    connect ControlAlarm with firebase

--- src/components/content/ControlAlarm.tsx
+++ src/components/content/ControlAlarm.tsx
@@ -6,6 +6,7 @@ import Typography from '@material-ui/core/Typography';
 import { Grid } from '@material-ui/core';
 import axios from "axios";
 import Button from '@material-ui/core/Button';
+import { async } from 'q';
 
 const styles = (theme: Theme) => createStyles({
     root: {
@@ -26,17 +27,46 @@ const styles = (theme: Theme) => createStyles({
     toggledButton: {
         fontSize: 20,
         color: "white",
-        background: "red !important" ,
+        background: "red !important",
     },
 
 });
 
 export interface Props extends WithStyles<typeof styles> { }
 
+
 const ControlAlarm = (props: Props) => {
     const { classes } = props;
     const [alarmLED, toggleAlarmLED] = useState(false);
     const [siren, toggleSiren] = useState(false);
+    const updateData = async (device: string) => {
+        const url = `https://heatman-b390e.firebaseio.com/${device}.json`;
+        const response = await axios.put(url, {state: device=="AlarmLED"?alarmLED?"OFF":"ON":siren?"OFF":"ON"});
+        console.log(response);
+    }
+    const initData =    async ()=>{
+            const response = await axios
+            .get("https://heatman-b390e.firebaseio.com/.json")
+            .then(
+                (res)=>{
+                    if(res.data.Siren.state=="ON"){
+                        toggleSiren(true);
+                    }
+                    else if(res.data.Siren.state=="OFF"){
+                        toggleSiren(false);
+                    }
+                    if(res.data.AlarmLED.state=="ON"){
+                        toggleAlarmLED(true);
+                    }
+                    else if(res.data.AlarmLED.state=="OFF"){
+                        toggleAlarmLED(false);
+                    }
+                }
+            )
+        }
+    useEffect(()=>{
+        initData();
+    },[]);
     return (
         <div>
             <Grid container justify="center" style={{ paddingTop: 10 }}>
@@ -46,14 +76,14 @@ const ControlAlarm = (props: Props) => {
                         <Grid item>
                             <Button variant="contained"
                                 className={alarmLED ? classes.toggledButton : classes.button}
-                                onClick={() => { toggleAlarmLED(!alarmLED) }}>
+                                onClick={() => { toggleAlarmLED(!alarmLED); updateData("AlarmLED") }}>
                                 Alarm LED
                             </Button>
                         </Grid>
                         <Grid item>
                             <Button variant="contained"
                                 className={siren ? classes.toggledButton : classes.button}
-                                onClick={() => { toggleSiren(!siren) }}>
+                                onClick={() => { toggleSiren(!siren); updateData("Siren");  }}>
                                 Siren
                                 </Button>
                         </Grid>
